dae32b5e6d86e881c811a1c773ad5d3f4a30d189
ravisabbi <ravisabbi7036@gmail.com>
2023-01-10 12:46:00 +0000

    blog list

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-containainer">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="role">Software Developer at UK</p>
+  </div>
+)
+export default UserInfo
